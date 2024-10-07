f4e7da91919d8309d6c25ceeae9cc90a349b0aac
Velotzem <kropivit2006@ukr.net>
2024-10-07 23:40:09 +0300

    Added file Lesson_1/Task_3/Task_3.pro and added Lesson_1/Task_3/main.cpp

---
+++ Lesson_1/Task_3/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    float ch1,ch2; char diya;
+    cout << "Enter the first number:" << endl;
+    cin >> ch1;
+    cout << "Enter the second number:" << endl;
+    cin >> ch2;
+    cout << "Enter the action you want to do with the numbers: +,-,*,/,^" << endl;
+    cin >> diya;
+    switch (diya) {
+    case '+':
+        cout << "The result of addition is : " << ch1+ch2 << endl;
+        break;
+    case '-':
+        cout << "The result of the subtraction is : " << ch1-ch2 << endl;
+        break;
+    case '*':
+        cout << "The result of the multiplication is : " << ch1*ch2 << endl;
+        break;
+    case '/':
+        if (ch2 == 0) {
+            cout << "You can't divide by 0!" << endl;
+        }
+        else {
+            cout << "The result of division is : " << ch1/ch2 << endl;}
+        break;
+    case '^':
+        cout << "the result of raising a number to a power is : " << pow(ch1,ch2) << endl;
+        break;
+    default:
+        cout << "You entered something wrong!" << endl;
+        break;
+    }
+    return 0;
+}
