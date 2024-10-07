72fdd553c1a03e6eb281a17b87d092c639ac2ac9
Velotzem <kropivit2006@ukr.net>
2024-10-08 00:52:43 +0300

    Added file Lesson_1/Task_4/Task_4.pro and added Lesson_1/Task_4/main.cpp

---
+++ Lesson_1/Task_4/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float zarpl;
+    cout << "How much do you earn in dollars?" << endl;
+    cin >> zarpl;
+    if (zarpl < 1000) {
+        cout << "You should work harder!" << endl;
+    }
+    if (1000 < zarpl) {
+        if (zarpl < 1000000) {
+            cout << "You are well done!" << endl;
+        }
+    }
+    if (zarpl > 1000000){
+        cout << "You are a millionaire!" << endl;
+    }
+    if (!(zarpl - 1000)) {
+        cout << "The salary is not small enough to say that you should work more, but not large enough to say that you are well done!" << endl;
+    }
+    if (!(zarpl - 1000000)) {
+        cout << "The salary is not so small enough to say that you well done, but not large enough to say that you are a millionaire!" << endl;
+    }
+    return 0;
+}
