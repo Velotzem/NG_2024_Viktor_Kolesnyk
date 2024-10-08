765a71cce1f7b5f978e72a52ee6c3dbbb5b0d647
Velotzem <kropivit2006@ukr.net>
2024-10-08 09:14:04 +0300

    Added file Lesson_1/Task_5/Task_5.pro and added Lesson_1/Task_5/main.cpp

---
+++ Lesson_1/Task_5/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int kilkist;
+    cout << "Enter a number that will be equal to the number of rows of the Christmas tree" << endl;
+    cin >> kilkist;
+    for(int i=0; i<kilkist; i++){
+        for(int j=1; j<kilkist-i;j++){
+             cout << " ";}
+        for(int j=1; j<=2*(i+1)-1;j++){
+            cout << "*";}
+        cout << "\n";}
+    if(kilkist > 1) {
+    int j=1;
+    while(j<kilkist){ //Використав while замість for, щоб було різноманіття
+        cout << " ";
+        j++;}
+    cout << "*\n";}
+    return 0;
+}
