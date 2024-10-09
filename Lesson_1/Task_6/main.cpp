2a41da166ac49d3dd5bf433d1b277dda5bd549ef
Velotzem <kropivit2006@ukr.net>
2024-10-09 23:40:43 +0300

    Modified file Lesson_1/Task_6/main.cpp

--- Lesson_1/Task_6/main.cpp
+++ Lesson_1/Task_6/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int kilkist = 0;
 
-void clear() {
+void clear() { //очищення, затримка + відступ вгорі
     Sleep(1000);
     if (system("cls")){
         system("clear");
@@ -14,15 +14,15 @@ void clear() {
         cout << "\n";
     }
 }
-void sycl1() {
+void sycl1() { //маленький відступ для трикутників справа
     for(int j=0; j<kilkist+1; j++){
         cout << "  ";}
 }
-void sycl2() {
+void sycl2() { //великий відступ для трикутників справа
     for(int j=0; j<kilkist*2; j++){
         cout << "  ";}
 }
-void vidstyp_dlya_sycliv(){
+void vidstyp_dlya_sycliv(){ //відступ вгорі для поворотів
     for(int i=1; i<kilkist; i++){
         cout << "\n";}
 }
@@ -33,21 +33,21 @@ int main()
     cin >> kilkist;
     while(1){
         clear();
-        for(int i=0; i<kilkist; i++){
+        for(int i=0; i<kilkist; i++){ //перший спрайт трикутника
             sycl2();
             for(int j=0; j<kilkist-(kilkist-i-1);j++){
                 cout << "* ";}
             cout << "\n";}
         clear();
         vidstyp_dlya_sycliv();
-        for(int i=0; i<kilkist; i++){
+        for(int i=0; i<kilkist; i++){ //другий спрайт трикутника
             sycl2();
             for(int j=0; j<kilkist-i;j++){
                 cout << "* ";}
             cout << "\n";}
         clear();
         vidstyp_dlya_sycliv();
-        for(int i=0; i<kilkist; i++){
+        for(int i=0; i<kilkist; i++){ //третій спрайт трикутника
             sycl1();
             for(int j=0; j<i*2;j++){
                 cout << " ";}
@@ -55,7 +55,7 @@ int main()
                 cout << "* ";}
             cout << "\n";}
         clear();
-        for(int i=0; i<kilkist; i++){
+        for(int i=0; i<kilkist; i++){ //четвертий спрайт трикутника
             sycl1();
             for(int j=0; j<(kilkist-i)*2-2;j++){
                 cout << " ";}
