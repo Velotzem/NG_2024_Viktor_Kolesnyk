a81fbc3b02a10044cd7b1c14268575ee58c2dcce
Velotzem <kropivit2006@ukr.net>
2024-10-11 21:35:25 +0300

    Modified file Task_4/main.cpp

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -10,19 +10,13 @@ int main()
     if (zarpl < 1000) {
         cout << "You should work harder!" << endl;
     }
-    if (1000 < zarpl) {
+    if (999 < zarpl) {
         if (zarpl < 1000000) {
             cout << "You are well done!" << endl;
         }
     }
-    if (zarpl > 1000000){
+    if (zarpl > 999999){
         cout << "You are a millionaire!" << endl;
-    }
-    if (!(zarpl - 1000)) {
-        cout << "The salary is not small enough to say that you should work more, but not large enough to say that you are well done!" << endl;
-    }
-    if (!(zarpl - 1000000)) {
-        cout << "The salary is not so small enough to say that you well done, but not large enough to say that you are a millionaire!" << endl;
     }
     return 0;
 }
