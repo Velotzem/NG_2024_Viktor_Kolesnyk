6720ff28e0e2a90e884c2922bc21735f73eaa00d
Velotzem <kropivit2006@ukr.net>
2024-10-16 18:20:25 +0300

    Modified file Lesson_1/Task_4/main.cpp

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main()
 {
-    float zarpl = 0;
+    float salary = 0;
     cout << "How much do you earn in dollars?" << endl;
-    cin >> zarpl;
-    if (zarpl < 1000) {
+    cin >> salary;
+    if (salary < 1000) {
         cout << "You should work harder!" << endl;
     }
-    if (999 < zarpl) {
-        if (zarpl < 1000000) {
+    else if (salary > 999) {
+        if (salary < 1000000) {
             cout << "You are well done!" << endl;
         }
     }
-    if (zarpl > 999999){
+    else if (salary > 999999){
         cout << "You are a millionaire!" << endl;
     }
     return 0;
