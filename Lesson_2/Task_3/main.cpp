cf82ffa19c499f43bc04f7f4a945d0246dbb3b38
Velotzem <kropivit2006@ukr.net>
2024-10-16 18:35:50 +0300

    Modified file Lesson_2/Task_3/main.cpp

--- Lesson_2/Task_3/main.cpp
+++ Lesson_2/Task_3/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int massif[20] = {0};
     int real_massif = 20, max = 0;
-    for (int number=0;number<20;number++) {
+    for (int number = 0;number < 20;number++) {
         cout << "Enter " << number+1 << " value:";
         cin >> massif[number];
         if (massif[number] == 0){
@@ -18,11 +18,11 @@ int main()
         }
     }
     cout << endl;
-    for (int columns=0;columns<real_massif;columns++){
-        for (int space=0;space<(max-massif[columns])/2;space++){ //Numbers cannot always make the same number of spaces on the left and right, so in such cases there will be 1 more on the right
+    for (int columns = 0;columns < real_massif;columns++){
+        for (int space = 0;space < (max-massif[columns])/2;space++){ //Numbers cannot always make the same number of spaces on the left and right, so in such cases there will be 1 more on the right
             cout << " ";
         }
-        for(int lines=0; lines < massif[columns];lines++){
+        for(int lines = 0; lines < massif[columns];lines++){
             cout << "*";
         }
         cout << "\n";
