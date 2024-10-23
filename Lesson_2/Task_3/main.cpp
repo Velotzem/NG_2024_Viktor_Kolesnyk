0a0632c67c358d16d82741aa2be3b95950411f36
Velotzem <kropivit2006@ukr.net>
2024-10-23 21:48:44 +0300

    Modified file Lesson_2/Task_3/main.cpp

--- Lesson_2/Task_3/main.cpp
+++ Lesson_2/Task_3/main.cpp
@@ -4,22 +4,23 @@ using namespace std;
 
 int main()
 {
-    int massif[20] = {0};
-    int real_massif = 20, max = 0;
-    for (int number = 0;number < 20;number++) {
-        cout << "Enter " << number+1 << " value:";
-        cin >> massif[number];
-        if (massif[number] == 0){
-            real_massif = number;
-            break;}
-        if (massif[number] > max)
+    const int size_massif = 20;
+    int massif[size_massif] = {0};
+    int number_of_array_massif  = size_massif, max = 0;
+    for (int point_number = 0;point_number < size_massif;point_number++) {
+        cout << "Enter " << point_number+1 << " value:";
+        cin >> massif[point_number];
+        if (massif[point_number] > max)
         {
-            max = massif[number];
+            max = massif[point_number];
         }
+        if (massif[point_number] == 0){
+            number_of_array_massif = point_number;
+            point_number = size_massif;}
     }
     cout << endl;
-    for (int columns = 0;columns < real_massif;columns++){
-        for (int space = 0;space < (max-massif[columns])/2;space++){ //Numbers cannot always make the same number of spaces on the left and right, so in such cases there will be 1 more on the right
+    for (int columns = 0;columns < number_of_array_massif;columns++){
+        for (int space = 0;space < (max - massif[columns]) / 2;space++){ //Numbers cannot always make the same number of spaces on the left and right, so in such cases there will be 1 more on the right
             cout << " ";
         }
         for(int lines = 0; lines < massif[columns];lines++){
