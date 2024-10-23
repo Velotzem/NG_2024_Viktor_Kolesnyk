fdc82750c35f5d4fdd22dd2328fa4f9d12bfb71a
Velotzem <kropivit2006@ukr.net>
2024-10-23 21:26:30 +0300

    Modified file Lesson_2/Task_1/main.cpp

--- Lesson_2/Task_1/main.cpp
+++ Lesson_2/Task_1/main.cpp
@@ -6,13 +6,13 @@ int main()
 {
     const int size_massif = 5; //It is enough to change the size of the array so that there is no comma at the end
     int massif[size_massif] = {0};
-    for (int point_number=0;point_number<5;point_number++) {
-        cout << "Enter " << point_number+1 << " value:" << endl;
+    for (int point_number = 0;point_number < 5;point_number++) {
+        cout << "Enter " << point_number + 1 << " value:" << endl;
         cin >> massif[point_number];
     }
-    for (int number=0;number<size_massif;number++) {
+    for (int number = 0;number < size_massif;number++) {
         cout << massif[number];
-        if (number<size_massif-1) {
+        if (number<size_massif - 1) {
             cout << ", ";
         }
     }
