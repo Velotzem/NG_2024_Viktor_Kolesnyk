5726b755866c2480fa0ed5271521fdd2416bc45c
Velotzem <kropivit2006@ukr.net>
2024-10-29 16:27:29 +0200

    Replaced strlen with a while loop in the file Lesson_2/Task_4/main.cpp

--- Lesson_2/Task_4/main.cpp
+++ Lesson_2/Task_4/main.cpp
@@ -6,9 +6,13 @@ int main() {
     const int max_length_text = 500; //set the size of the string
     char text[max_length_text] = {0};
     int count[128] = {0}; //a larger array could be used here, but I'm not using Windows 1251
+    int length_of_text = 0;
     cout << "Enter a string: ";
     cin.getline(text, max_length_text);
-    for (int array_element_number = 0;array_element_number < strlen(text);array_element_number++) { //it writes in the array count +1 to the value of the numerical values ​​of the char characters
+    while(text[length_of_text] != '\0'){ //finding the length of a string
+        length_of_text++;
+    }
+    for (int array_element_number = 0;array_element_number < length_of_text; array_element_number++) { //it writes in the array count +1 to the value of the numerical values ​​of the char characters
         count[(int)text[array_element_number]]++;
     }
     cout << "Number of different characters:\n";
