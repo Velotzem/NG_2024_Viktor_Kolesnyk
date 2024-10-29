528d3f7122454be2c4b104895bd8120ead5f2e89
Velotzem <kropivit2006@ukr.net>
2024-10-29 16:33:03 +0200

    Replaced strlen with a while loop in the file Lesson_2/Task_5/main.cpp and verified that the code works

--- Lesson_2/Task_5/main.cpp
+++ Lesson_2/Task_5/main.cpp
@@ -5,10 +5,13 @@ using namespace std;
 int main() {
     const int max_length_text = 500; //set the size of the string
     char text[max_length_text] = {0};
-    int count_all_alphabetic_characters = 0, word_count = 0;
+    int count_all_alphabetic_characters = 0, word_count = 0, length_of_text = 0;
     cout << "Enter a string: ";
     cin.getline(text, max_length_text);
-    for (int array_element_number = 0; array_element_number < strlen(text); array_element_number++) {
+    while(text[length_of_text] != '\0'){ //finding the length of a string
+        length_of_text++;
+    }
+    for (int array_element_number = 0; array_element_number < length_of_text; array_element_number++) {
         if((text[array_element_number] > 64 && text[array_element_number] < 91)
             || (text[array_element_number] > 96 && text[array_element_number] < 123)){
             count_all_alphabetic_characters += 1;
