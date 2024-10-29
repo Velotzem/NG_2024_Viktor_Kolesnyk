f7a78a53776e89136f559dd35edec3d2160d4c4c
Velotzem <kropivit2006@ukr.net>
2024-10-29 14:28:39 +0200

    Replaced line 17,18,19 and for with while file Lesson_2/Task_4/main.cpp

--- Lesson_2/Task_3/main.cpp
+++ Lesson_2/Task_3/main.cpp
@@ -6,18 +6,18 @@ int main()
 {
     const int size_massif = 20;
     int massif[size_massif] = {0};
-    int number_of_array_massif  = size_massif, max = 0;
-    for (int point_number = 0;point_number < size_massif;point_number++) {
+    int number_of_array_massif  = size_massif, max = 0, point_number = 0;
+    do
+    {
         cout << "Enter " << point_number+1 << " value:";
         cin >> massif[point_number];
         if (massif[point_number] > max)
         {
             max = massif[point_number];
         }
-        if (massif[point_number] == 0){
-            number_of_array_massif = point_number;
-            point_number = size_massif;}
+        point_number++;
     }
+    while (point_number < size_massif && massif[point_number-1] != 0);
     cout << endl;
     for (int columns = 0;columns < number_of_array_massif;columns++){
         for (int space = 0;space < (max - massif[columns]) / 2;space++){ //Numbers cannot always make the same number of spaces on the left and right, so in such cases there will be 1 more on the right
